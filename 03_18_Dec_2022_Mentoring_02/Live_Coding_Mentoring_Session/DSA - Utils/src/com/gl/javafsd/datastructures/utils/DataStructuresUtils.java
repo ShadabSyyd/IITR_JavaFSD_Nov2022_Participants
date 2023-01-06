8137ea3be70357e6bf45a1737fc1847d28652a79
joycee-gl <joycee.itwork.gl@gmail.com>
2023-01-06 17:53:26 +0530

    files

--- 03_18_Dec_2022_Mentoring_02/Live_Coding_Mentoring_Session/DSA - Utils/src/com/gl/javafsd/datastructures/utils/DataStructuresUtils.java
+++ 03_18_Dec_2022_Mentoring_02/Live_Coding_Mentoring_Session/DSA - Utils/src/com/gl/javafsd/datastructures/utils/DataStructuresUtils.java
@@ -72,4 +72,20 @@ public class DataStructuresUtils {
 		System.out.println(" }");
 	}
 	
+	public static String concatenate(int[] array) {
+		
+		StringBuilder result = new StringBuilder();
+		result.append("{ ");
+		for (int index = 0; index < array.length; index ++) {	
+
+			result.append(array[index]);
+
+			if (index != (array.length - 1)) {
+				result.append(", ");
+			}
+		}
+		result.append(" }");
+		return result.toString();
+	}
+	
 }
